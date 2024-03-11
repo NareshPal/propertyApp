22a0099ead01f0515bb1be975d013948dc02a420
naresh pal <nareshpal@nareshs-MacBook-Air.local>
2024-03-11 18:55:21 +0530

    Hero section static added

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
+import Hero from "@/components/Hero";
 import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
-      <div className="text-blue-600 text-3xl">HomePage!</div>
-      <Link href={"/properties"}>Properties</Link>
+      <Hero />
     </>
   );
 };
