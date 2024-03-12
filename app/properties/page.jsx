42afc530742400e8c151c448ac86fe51c3830d20
naresh pal <nareshpal@nareshs-MacBook-Air.local>
2024-03-12 17:57:32 +0530

    properties list added from json file

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
   return (
-    <>
-      <div className="text-blue-600 text-3xl">HomePage!</div>
-      <Link href={"/"}>Home</Link>
-    </>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.length === 0 ? (
+            "No Property Found"
+          ) : (
+            <>
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </section>
   );
 };
 
